95408c5efcb68575fb773f01aa9f4e4dc1f5feb2
minho <dnjsalsgh123@gmail.com>
2022-05-01 17:47:09 +0900

    Home 페이지에서 상품이미지, 상품 가격, 카테고리, 이름 출력

---
+++ src/test/java/test/service/MemberServiceTest.java
@@ -0,0 +1,28 @@
+package test.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class MemberServiceTest {
+
+//    @Autowired
+//    MemberRepository memberRepository;
+//
+//    @Test
+//    void findbyLoginid(){
+////        Optional<MemberFormDto> byMemberLoginid = memberRepository.findByLoginId("1234");
+////        System.out.println("byMemberLoginid = " + byMemberLoginid);
+//    }
+//
+//    @Test
+//    void findAll() {
+//
+//        List<Members> all = memberRepository.findAll();
+//        System.out.println("all = " + all);
+//        //조회
+//    }
+}
