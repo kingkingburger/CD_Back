45c094e575765b3620ee23080c064c10287a71cd
minho <dnjsalsgh123@gmail.com>
2022-05-21 10:04:59 +0900

    Cart entity 생성

---
+++ src/main/java/SilkLoad/entity/Cart.java
@@ -0,0 +1,32 @@
+package SilkLoad.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Data
+@AllArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "BASKET_ID")
+    private Long id; // PK
+
+
+    @OneToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Members memberid;  //구매자의 id
+
+    @OneToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product productid; //물품의 id
+
+    private LocalDate createDate = LocalDate.now();
+}
