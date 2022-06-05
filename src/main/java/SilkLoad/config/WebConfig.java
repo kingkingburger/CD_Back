ee1c21f58f0264e57a084096c05c62c33ac05b89
kang junho <0320kangk@naver.com>
2022-06-05 18:56:23 +0900

    webconfig sub,pub ¼öÁ¤

--- src/main/java/SilkLoad/config/WebConfig.java
+++ src/main/java/SilkLoad/config/WebConfig.java
@@ -21,7 +21,6 @@ public class WebConfig implements WebMvcConfigurer {
                         "/members/add","/members/myPage/profile","/members/myPage/wishlist","/members/myPage/saleOrders",
                         "/members/myPage/purchaseOrders","/product/order/transaction/**", "/members/myPage/myChatRoomList",
                         "/members/myPage/room/**",
-                        "/stomp/chat/**","/puh/**","/sub/**",
                         "/login", "/logout","/home",
                         "/product/add","/product/order/buyNow","/product/order/inCart","/product/images/**","/product/order/buyAuction",
                        "/product/order/deleteCart",
