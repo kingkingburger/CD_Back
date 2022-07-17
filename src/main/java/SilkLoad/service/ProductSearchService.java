44ee4f4dacd65767a745426e9ff10bead1ca9f29
minho <dnjsalsgh123@gmail.com>
2022-07-17 09:47:17 +0900

    productService 리팩토링(?)

---
+++ src/main/java/SilkLoad/service/ProductSearchService.java
@@ -0,0 +1,33 @@
+package SilkLoad.service;
+
+import SilkLoad.dto.ProductRecordDto;
+import SilkLoad.entity.ProductEnum.ProductType;
+import SilkLoad.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductSearchService {
+    private final ProductRepository productRepository;
+    private final ProductService productService;
+    /**
+     * keyword를 기준으로 Paging된 productRecordDto를 반환
+     *
+     * @param keyword
+     * @param pageable
+     * @return
+     */
+    @Transactional
+    public Page<ProductRecordDto> SearchToProductname(String keyword, Pageable pageable) {
+        Page<ProductRecordDto> productRecordDtoPage = productRepository
+                .findByNameContainingIgnoreCaseAndProductType(keyword, ProductType.sale, pageable)
+                .map(productService::ProductToProductRecordDto);
+        return productRecordDtoPage;
+    }
+}
