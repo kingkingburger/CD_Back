f63e57784305294db71ba0cd432065da1957bc22
Shin Yebeen <shinyebeen1126@gmail.com>
2022-07-27 02:49:57 +0900

    유효성 검사

--- src/main/java/SilkLoad/controller/Member/MemberController.java
+++ src/main/java/SilkLoad/controller/Member/MemberController.java
@@ -2,8 +2,10 @@ package SilkLoad.controller.Member;
 
 import SilkLoad.dto.MemberFormDto;
 import SilkLoad.service.MemberService;
+import com.sun.tools.jconsole.JConsoleContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.sql.SQLIntegrityConstraintViolationException;
 
 @Slf4j
 @Controller
@@ -31,10 +34,18 @@ public class MemberController {
     @PostMapping("/add")
     public String save(@Valid @ModelAttribute MemberFormDto form,
                        BindingResult bindingResult) {
+
         if (bindingResult.hasErrors()) {
             return "addMemberForm";
         }
-        memberService.save(form);
+
+        try{
+            memberService.save(form);
+        }catch (DataIntegrityViolationException e){
+            bindingResult.reject("memberAddFail", "이미 존재하는 아이디입니다.");
+            return "addMemberForm";
+        }
+
         return "redirect:/";
     }
 
