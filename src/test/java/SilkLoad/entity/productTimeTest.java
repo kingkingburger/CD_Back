f10cd68994a3c0dce6a9997bec9399f79e75d387
minho <dnjsalsgh123@gmail.com>
2022-08-11 08:41:42 +0900

    aws랑 연동 상태

--- src/test/java/SilkLoad/entity/productTimeTest.java
+++ src/test/java/SilkLoad/entity/productTimeTest.java
@@ -14,24 +14,17 @@ class productTimeTest {
 
     @Test
     void enumTest() {
-
         ProductTime x = ProductTime.ONE_DAY;
         Assertions.assertThat(x).isEqualTo(ProductTime.ONE_DAY);
         System.out.println(x.getDescription());
-
     }
 
     @Test
     void LocalTimeTest() {
-
         LocalDateTime now = LocalDateTime.now();
-
         if (now.isBefore(now.plusDays(2))) {
-            log.info("시간 테스트 통과={}", now.plusDays(2));
+//            log.info("시간 테스트 통과={}", now.plusDays(2));
         }
-
-
-
     }
 
 
